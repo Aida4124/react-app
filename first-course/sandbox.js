d882b7532b0baa22d257931f108af40747bdf0e1
Ayda2234 <202032009633@etu.univ-chlef.dz>
2024-09-25 22:02:55 +0200

    comit

---
+++ first-course/sandbox.js
@@ -0,0 +1,17 @@
+// let is we want to change the variable between line of codes 
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+age = 30;
+
+console.log(age, year);
+
+year = 2018;
+console.log(age, year);
+
+//const for not allowed to change the variable  
+
+const points = 100;
+points = 50;
+console.log(points);
